b50d5f7e63d7307418b23ccb8165dde049e048a2
Diego Sáinz de Medrano <diego.sainz101@alu.ulpgc.es>
2017-01-06 20:51:48 +0000

    Mostly working: users and travels. Will have to redo redcarpet support.

---
+++ app/models/travel.rb
@@ -0,0 +1,6 @@
+class Travel < ApplicationRecord
+  validates :title, length: {in: 4..40}, uniqueness: {case_sensitive: false}
+  validates :content, length: {in: 150..7000}
+
+  belongs_to :user
+end
