6245761e090cd3797f0ab16b11cded7e6988cc2d
Diego Sáinz de Medrano <diego.sainz101@alu.ulpgc.es>
2017-01-08 13:35:26 +0000

    Work in styling. Added travel partial.

--- db/schema.rb
+++ db/schema.rb
@@ -12,14 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20170107231913) do
 
-  create_table "profiles", force: :cascade do |t|
-    t.date     "birthdate"
-    t.text     "bio"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "travels", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
