5efdc0e312139a5bb8593a8f8914e4ef752592d8
Diego Sáinz de Medrano <diego.sainz101@alu.ulpgc.es>
2017-01-08 01:35:58 +0000

    A lot of work done in styling. Added the preview slider.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def home
     @last_travel = Travel.published.last
+    @slides_providers = Travel.published.last(4)
   end
 
   def about
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170107112239) do
+ActiveRecord::Schema.define(version: 20170107231913) do
+
+  create_table "profiles", force: :cascade do |t|
+    t.date     "birthdate"
+    t.text     "bio"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "travels", force: :cascade do |t|
     t.string   "title"
@@ -37,6 +45,8 @@ ActiveRecord::Schema.define(version: 20170107112239) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "name"
+    t.text     "bio"
+    t.string   "webpage"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
